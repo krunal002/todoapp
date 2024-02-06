03010d7b57adedca5113eae9fa29bb70578e91cf
krunal002 <109591661+krunal002@users.noreply.github.com>
2024-02-06 18:05:28 +0530

    Input Fields added

--- src/ToDoApp/todo.jsx
+++ src/ToDoApp/todo.jsx
@@ -3,6 +3,23 @@ const ToDoApp = () => {
     <main>
       <header>
         <h1>ToDo App</h1>
+        <div className="add-todo">
+          <input
+            type="text"
+            name="title"
+            id="title"
+            className="input-field"
+            placeholder="Add new task..."
+          />
+
+          <input
+            type="text"
+            name="time"
+            id="time"
+            className="input-field"
+            placeholder="Start time (e.g. 3pm)"
+          />
+        </div>
       </header>
     </main>
   );
